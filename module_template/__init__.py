d28f04cc6c16ad4102d7256fd9b739b17c4c6dfc
Matt Krueger <mkrueger@rstms.net>
2021-08-24 16:51:34 -0500

    fix typo in __init__.py docstring

--- module_template/__init__.py
+++ module_template/__init__.py
@@ -1,12 +1,12 @@
-"""{{ project.name }} - {{ project.decription.short }}
+"""{{ project.name }} - {{ project.description.short }}
 
 {{ project.description.medium }}
 
 """
+from .cli import cli
 from .context import Context
 from .exception import ParameterError
 from .constant import status
-from .cli import cli
 from .version import __version__, __timestamp__, __header__
 
 __all__ = ['Context' 'ParameterError', 'status', 'cli', '__version__']
